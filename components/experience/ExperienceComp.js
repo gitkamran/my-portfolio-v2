86a94df80fff7635ed963f856c79e95d557f1167
gitkamran <mkamran.ir@gmail.com>
2026-09-21 03:06:15 +0330

    add experience component

---
+++ components/experience/ExperienceComp.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ExperienceComp = () => {
+  return (
+    <div className="flex items-center justify-center w-full h-full">
+      سوابق شغلی
+    </div>
+  );
+};
+
+export default ExperienceComp;
