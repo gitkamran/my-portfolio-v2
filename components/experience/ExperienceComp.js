f63b85f59c8702e4f0a453504ae0f493278c913e
gitkamran <mkamran.ir@gmail.com>
2026-09-21 22:11:56 +0330

    complete experience

--- components/experience/ExperienceComp.js
+++ components/experience/ExperienceComp.js
@@ -1,9 +1,23 @@
+import { getExperience } from "@/app/lib/data";
 import React from "react";
+import ExperienceItem from "./ExperienceItem";
 
 const ExperienceComp = () => {
+  const experience = getExperience();
   return (
-    <div className="flex items-center justify-center w-full h-full">
-      سوابق شغلی
+    <div className="flex flex-col items-center justify-center gap-0 max-w-md md:max-w-xl lg:max-w-2xl xl:max-w-3xl my-14 md:my-20 px-4 md:px-0 mx-auto">
+      {experience.map((e) => (
+        <ExperienceItem
+          key={e.id}
+          company={e.company}
+          role={e.role}
+          period={e.period}
+          location={e.location}
+          description={e.description}
+          highlights={e.highlights}
+          tech={e.tech}
+        />
+      ))}
     </div>
   );
 };
